23e5c78db1bef70161eac76e5df093f772792f1d
terngkub <terngnattapol@gmail.com>
2018-04-02 19:03:11 +0200

    add nav bar

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const sass        = require('gulp-sass');
 
 // Move JS Files to dest/js
 gulp.task('js', function() {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
+    return gulp.src(['src/js/*', 'node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
     	.pipe(gulp.dest('dist/js'))
 });
 
